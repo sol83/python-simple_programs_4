5796c5d26e1bdc7782504707990717b8314f06c6
Solon P <freeberd83@gmail.com>
2021-05-13 18:49:20 +0300

    Add files via upload

---
+++ Image Manipulation/red_noise.py
@@ -0,0 +1,32 @@
+import random
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/simba-sq.jpg'
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the image before the transform
+    image.show()
+
+    # Apply the filter
+    # TODO: your code here!
+    for pixel in image:
+        pixel.red = random.randint(0, 255)
+
+    # Show the image after the transform
+    image.show()
+
+
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter for default): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
